1dd917d48a4fa8d5d74de0fc47252767ac639712
KAM <1479376017@qq.com>
2021-05-07 20:31:48 +0800

    refactor: pm2.config.js

--- pm2.config.js
+++ pm2.config.js
@@ -1,28 +1,28 @@
 module.exports = {
 	apps: [
 		{
-			name: "web-blog",
+			name: "BiuBiu Blog",
 			script: "next start",
 			cwd: "./", // 当前工作路径
-			watch: [
-				".next" // 监控变化的目录，一旦变化，自动重启
-			],
+			// watch: [
+			// 	".next" // 监控变化的目录，一旦变化，自动重启
+			// ],
 			ignore_watch: [
 				// 从监控目录中排除
 				"node_modules",
 				"logs",
 				"static"
 			],
-			instances: 5,  // 启动2个实例
+			instances: 2,  // 启动2个实例
 			node_args: "--harmony",
-			env: {
-				NODE_ENV: "development",
-				PORT: 3000
-			},
-			env_production: {
-				NODE_ENV: "production",
-				PORT: 5000
-			},
+			// env: {
+			// 	NODE_ENV: "development",
+			// 	PORT: 3000
+			// },
+			// env_production: {
+			// 	NODE_ENV: "production",
+			// 	PORT: 5000
+			// },
 			out_file: "./logs/out.log", // 普通日志路径
 			error_file: "./logs/err.log", // 错误日志路径
 			merge_logs: true,
